8a78b6d83152dda60f93c3f88cd9cfdde294c1e3
Kai Chang <kai.h.chang@gmail.com>
2012-10-01 22:57:17 -0500

    Donation amount validation.

--- public/js/global.js
+++ public/js/global.js
@@ -7,8 +7,10 @@
       $("div.modal-body").load('/donate .donate-content', function() {
         $("div.modal").modal();
       });
-      $("#submit-donation").on('click', function() {
-        $("#payment-form").submit();
+      $("#submit-donation").on('click', function(e) {
+        if ($("#braintree_amount").val() > 0) {
+          $("#payment-form").submit();
+        }
       });
     })
   });
