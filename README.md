04bfa76a3ab4e73e0ab2138a82b80cf23cbe53a0
Kai Chang <kai.h.chang@gmail.com>
2012-10-01 23:26:55 -0500

    Add unit test cases for all routes.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-Help me buy my favorite albums.
+Help me buy my favorite albums!
+
+Run:
+node app
+
+Run unit tests:
+mocha --reporter spec
--- test/controller-main.js
+++ test/controller-main.js
@@ -1,5 +1,6 @@
 var assert = require("assert")
   , express = require('express')
+  , braintree = require('braintree')
   , _ = require('underscore');
 
 describe("Main Controller", function() {
@@ -26,6 +27,10 @@ describe("Main Controller", function() {
     it("should have a default route: /", function() {
       assert.equal(isRoute('get', '/'), true, "Default route is missing");      
     });
+
+    it("should have a view route to see other user's top albums: /view", function() {
+      assert.equal(isRoute('get', '/view'), true, "View route is missing");
+    });
     
     it("should have an about route: /about", function() {
       assert.equal(isRoute('get', '/about'), true, "About route is missing");
@@ -33,6 +38,14 @@ describe("Main Controller", function() {
     
     it("should have a contact route: /contact", function() {
       assert.equal(isRoute('get', '/contact'), true, "Contact route is missing");
+    });
+
+    it("should have a donate route: /donate", function() {
+      assert.equal(isRoute('get', '/donate'), true, "Donate route is missing");
+    });
+
+    it("should have a thanks route: /thanks", function() {
+      assert.equal(isRoute('get', '/thanks'), true, "Thanks route is missing");
     });
   });
 });
